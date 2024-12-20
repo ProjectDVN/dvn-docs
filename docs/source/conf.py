e3cef7d5fb210269facf119195126685a615ae4f
bauss-dev <jacobbauss@gmail.com>
2024-12-20 14:57:33 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Project DVN'
+copyright = '2024, Project DVB'
+author = 'Project DVN'
 
 release = '0.1'
-version = '0.1.0'
+version = '0.1.9'
 
 # -- General configuration
 
